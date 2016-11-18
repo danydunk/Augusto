eee7d4419c58391f2a5c884392876d1b9b7a34f4
danydunk <daniele.zuddas@tiscali.it>
2016-11-18 09:51:06 -0800

    fixed travis

--- files/for_test/SUTs_configuration/upm_reset.bat
+++ files/for_test/SUTs_configuration/upm_reset.bat
@@ -1,5 +1,5 @@
 rmdir C:\Users\usi\abtMyDocuments\ /Q /S
 rem C:\Users\usi\upm.properties /Q
 mkdir C:\Users\usi\abtMyDocuments\
-copy C:\workspace\GUISemantics_RFT\files\for_test\SUTs_configuration\upm_db C:\Users\usi\abtMyDocuments\upm_db
-copy C:\workspace\GUISemantics_RFT\files\for_test\SUTs_configuration\upm.properties C:\Users\usi\upm.properties
+copy .\files\for_test\SUTs_configuration\upm_db C:\Users\usi\abtMyDocuments\upm_db
+copy .\files\for_test\SUTs_configuration\upm.properties C:\Users\usi\upm.properties
