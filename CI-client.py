b8ad8347fd22e71fae36df480a0e197dc21440ac
danydunk <daniele.zuddas@tiscali.it>
2016-11-18 09:16:40 -0800

    setup CI

---
+++ CI-client.py
@@ -0,0 +1,31 @@
+# Echo client program
+import socket
+import sys
+
+HOST = 'research.inf.usi.ch'    # The remote host
+PORT = 50000              # The same port as used by the server
+s = None
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        s = None
+        continue
+    try:
+        s.connect(sa)
+    except socket.error as msg:
+        s.close()
+        s = None
+        continue
+    break
+if s is None:
+    print 'could not open socket'
+    sys.exit(1)
+s.sendall($TRAVIS_COMMIT)
+data = s.recv(1024)
+s.close()
+if data == "ok":
+    exit(0)
+else:
+    exit(-1)
