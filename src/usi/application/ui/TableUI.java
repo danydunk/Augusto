3f6d965a074e98cd291ee52d3212eb825826f3f0
danydunk <daniele.zuddas@tiscali.it>
2016-12-02 03:28:59 -0800

    small fix

--- src/usi/application/ui/TableUI.java
+++ src/usi/application/ui/TableUI.java
@@ -16,7 +16,7 @@ public class TableUI extends MainHelper {
 		// final int selectedColumn = cell % columnNumber;
 
 		new GuiSubitemTestObject(to).click(atCell(atRow(cell), atColumn(0)));
-		Thread.sleep(100);
+		Thread.sleep(200);
 		new GuiSubitemTestObject(to).click(atCell(atRow(cell), atColumn(0)));
 	}
 
