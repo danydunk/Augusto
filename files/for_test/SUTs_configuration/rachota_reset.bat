8a62e703a96cf17d8fb647f1946df598b54cf58b
danydunk <daniele.zuddas@tiscali.it>
2016-12-03 11:20:08 -0800

    added reset for rachota

---
+++ files/for_test/SUTs_configuration/rachota_reset.bat
@@ -0,0 +1,4 @@
+Taskkill.exe /F /IM firefox.exe
+Taskkill.exe /F /IM iexplore.exe
+del /Q C:\Users\usi\.rachota\*
+copy .\files\for_test\SUTs_configuration\settings.cfg C:\Users\usi\.rachota\settings.cfg
