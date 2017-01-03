7890dfc8cb6551a48b4751790934630c5ebb89e3
danydunk <daniele.zuddas@tiscali.it>
2017-01-03 09:57:43 -0800

    fixed buddi reset

--- files/for_test/SUTs_configuration/buddi_reset.bat
+++ files/for_test/SUTs_configuration/buddi_reset.bat
@@ -9,11 +9,11 @@ del /Q C:\Users\usi\AppData\Roaming\Buddi\Languages\*
 set targetdir=C:\Users\usi\AppData\Roaming\Buddi\Languages
 for /d %%x in (%targetdir%\*) do @rd /S /Q %%x
 for /d %%p in (C:\Users\usi\abtMyDocuments\\*) Do rd /Q /S "%%p"
-copy C:\workspace\Augusto\files\for_test\SUTs_configuration\Languages\* C:\Users\usi\AppData\Roaming\Buddi\Languages\*
+copy .\files\for_test\SUTs_configuration\Languages\* C:\Users\usi\AppData\Roaming\Buddi\Languages\*
 del /Q C:\Users\usi\abtMyDocuments\*.buddi3autosave
 del /Q C:\Users\usi\AppData\Roaming\Buddi\*.buddi3autosave
 del /Q C:\Users\usi\AppData\Roaming\Buddi\*autosave
 del /Q C:\Users\usi\AppData\Roaming\Buddi\*bak
 del /Q C:\Users\usi\abtMyDocuments\*bak
 del /Q C:\Users\usi\abtMyDocuments\*
-copy C:\workspace\Augusto\files\for_test\SUTs_configuration\buddidata.buddi3 C:\Users\usi\abtMyDocuments\
+copy .\files\for_test\SUTs_configuration\buddidata.buddi3 C:\Users\usi\abtMyDocuments\
