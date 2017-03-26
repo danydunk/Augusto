d06653ee87e2d56e4986a0a36c1c2c49e413e01f
danydunk <daniele.zuddas@tiscali.it>
2017-03-26 03:21:26 -0700

    fix CI

--- src/usi/testcase/AlloyTestCaseGenerator.java
+++ src/usi/testcase/AlloyTestCaseGenerator.java
@@ -148,9 +148,14 @@ class RunCommandThread extends Thread {
 
 		try {
 			int time = -1;
-			if (!this.inst.getSemantics().getRun_commands().get(0).contains(" for ")
-					|| !this.inst.getSemantics().getRun_commands().get(0).split(" for ")[1]
-							.contains("Time")) {
+			if ((!this.inst.getSemantics().getRun_commands().get(0).contains(" for ") || !this.inst
+					.getSemantics().getRun_commands().get(0).split(" for ")[1].contains("Time"))
+					&& (!this.inst.getSemantics().getRun_commands().get(0).contains("}for ") || !this.inst
+							.getSemantics().getRun_commands().get(0).split("}for ")[1]
+									.contains("Time"))
+					&& (!this.inst.getSemantics().getRun_commands().get(0).contains(")for ") || !this.inst
+							.getSemantics().getRun_commands().get(0).split(")for ")[1]
+							.contains("Time"))) {
 				time = this.time;
 			}
 			this.tc = AlloyUtil.getTestcase(this.inst, 0, 0,
